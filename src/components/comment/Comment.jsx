6f67f718c8f851ee2824288c9b15ee6f1b9f8545
zingrad <erick.isabelle@gmail.com>
2024-11-08 11:37:09 -0300

    [FEAT] like function in post

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -2,11 +2,18 @@ import { Trash } from "@phosphor-icons/react";
 import styles from "./Comment.module.css";
 import { ThumbsUp } from "@phosphor-icons/react/dist/ssr";
 import { Avatar } from "../Avatar/Avatar";
+import { useState } from "react";
 
 export function Comment({content,onDeleteComment}) {
 
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -31,9 +38,9 @@ export function Comment({content,onDeleteComment}) {
         </div>
 
         <footer>
-            <button>
+            <button onClick={handleLikeComment}>
                 <ThumbsUp />
-                Aplaudir <span>20</span>
+                Aplaudir <span>{likeCount}</span>
             </button>
         </footer>
       </div>
