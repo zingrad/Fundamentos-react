a806405b6e7ef7aca7c7da7131b100b4dff0681f
zingrad <erick.isabelle@gmail.com>
2024-11-20 21:58:17 -0300

    [FEAT] tipagem posts

--- src/App.tsx
+++ src/App.tsx
@@ -6,36 +6,53 @@ import styles from './App.module.css';
 
 import './global.css'
 
-const posts = [
+interface Author {
+  avatarUrl: string;
+  name: string;
+  role: string;
+}
+
+interface Content {
+  type: "link" | "paragraph";
+  content: string;
+}
+
+interface Post {
+  id: number;
+  author: Author;
+  content: Content[];
+  publishedAt: Date;
+}
+
+const posts: Post[] = [
   {
     id: 1,
     author: {
-      avatarUrl:'https://avatars.githubusercontent.com/u/85195379?v=4',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/85195379?v=4',
       name: 'Erick',
       role: 'Front End'
     },
     content: [
-        {type: 'paragraph', content:  'Opa beleza??',},
-        {type: 'paragraph', content:  'Estou estudando React'},
-        {type: 'link', content:  '@ErickGomes'},
+      { type: 'paragraph', content: 'Opa beleza??' },
+      { type: 'paragraph', content: 'Estou estudando React' },
+      { type: 'link', content: '@ErickGomes' },
     ],
     publishedAt: new Date('2024-09-25 20:00:00'),
   },
   {
     id: 2,
     author: {
-      avatarUrl:'https://avatars.githubusercontent.com/u/6643122?v=4',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/6643122?v=4',
       name: 'Maike',
       role: 'backend',
     },
     content: [
-        {type: 'paragraph', content:  'Fala galeraa 👋',},
-        {type: 'paragraph', content:  'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz,  no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-        {type: 'link', content:  '@mikeBrito'},
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz, no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '@mikeBrito' },
     ],
     publishedAt: new Date('2024-09-26 20:00:00'),
   }
-
 ];
 
 
